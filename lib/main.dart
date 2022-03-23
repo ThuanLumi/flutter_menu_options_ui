108ee55fb8fbaa3edc7d49839c50ef90efd8b6b6
Thuan Dinh Nam <thuandinhnam307@gmail.com>
2022-03-23 14:45:13 +0700

    Add flutter_template_menu_options_ui folder

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_template_menu_options_ui/screens/menu_options_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Select Option UI',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primaryColor: Colors.black),
+      home: MenuOptionsScreen(),
+    );
+  }
+}
